886a418c6dd8f664083030cb6899dfe21b36ff2c
Sergii Piatakov <sergii.piatakov@globallogic.com>
2018-11-15 15:21:34 +0200

    improve calculation accuracy
    Allow using float point arguments to avoid truncation.
    
    Test: Add (4.9, 4.9) should return 10.
    Signed-off-by: Sergii Piatakov <sergii.piatakov@globallogic.com>
    
    fix truncation error
    
    To convert float to integer the truncation is performed, but the
    rounding is expected.
    
    Test: Add (4.9, 4.9) should return 10.
    Signed-off-by: Sergii Piatakov <sergii.piatakov@globallogic.com>
    Signed-off-by: xczcv <alnazarchuk47@gmail.com>

--- calculator.h
+++ calculator.h
@@ -3,9 +3,15 @@
 
 class Calculator
 {
+<<<<<<< HEAD
 	public:
 		int Add (double, double);
 		int Sub (double, double);
+=======
+    public:
+        int Add (double, double);
+        int Sub (double, double);
+>>>>>>> b5fe945 (improve calculation accuracy)
 };
 
 #endif//CALCULATOR_H
